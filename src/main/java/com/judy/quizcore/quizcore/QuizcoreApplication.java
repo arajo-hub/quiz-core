e209bea0ffe076ab886a0e8efeb99a6c0118964c
arajo-hub <joara9566@naver.com>
2025-09-04 17:57:49 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/judy/quizcore/quizcore/QuizcoreApplication.java
@@ -0,0 +1,13 @@
+package com.judy.quizcore.quizcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizcoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/judy/quizcore/quizcore/QuizcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.judy.quizcore.quizcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
